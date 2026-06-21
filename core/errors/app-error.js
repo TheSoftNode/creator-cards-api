25f9a38ba446cb186c91f0ca573caf3e8f891a32
TheSoftNode <thesoftnode@gmail.com>
2026-06-21 17:55:20 +0100

    Extend error handling to support custom error codes
    Modified app-error to accept custom code as 3rd parameter. Added BADREQUEST and FORBIDDEN error codes. Updated server to include code field in error responses.

---
+++ core/errors/app-error.js
@@ -0,0 +1,32 @@
+/**
+ * Throw an app error
+ * @param {String} errorMessage
+ * @param {String} [errorCode]
+ * @param {{context:any, details:any, code:string}|string} [options]
+ */
+function appError(errorMessage, errorCode = 'ERR', options = {}) {
+  const error = new Error(errorMessage);
+  error.isApplicationError = true;
+  error.errorCode = errorCode;
+
+  // Support passing custom code as third parameter (string) or in options object
+  if (typeof options === 'string') {
+    error.code = options;
+  } else {
+    if (options.context) {
+      error.context = options.context;
+    }
+
+    if (options.details) {
+      error.details = options.details;
+    }
+
+    if (options.code) {
+      error.code = options.code;
+    }
+  }
+
+  throw error;
+}
+
+module.exports = appError;
